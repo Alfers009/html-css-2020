527f0964c8fe5a6f156e84f6ef60e0b6f8b68bda
Alfers009 <74427063+Alfers009@users.noreply.github.com>
2021-04-13 00:11:21 +0300

    13.4

---
+++ botega/botega.js
@@ -0,0 +1,13 @@
+document.querySelector ('#enter').addEventListener ('click', showLastPage)
+document.querySelector ('#back').addEventListener ('click', showHomePage)
+
+function showLastPage() {
+    document.querySelector('#lastpage').classList.add('slide-in')
+    document.querySelector('#lastpage').classList.remove('slide-out')
+}
+
+function showHomePage() {
+    document.querySelector('#lastpage').classList.add('slide-out')
+    document.querySelector('#lastpage').classList.remove('slide-in')
+    
+}
