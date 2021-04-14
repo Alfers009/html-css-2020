9b7884234da66fd3146d91977d73c6c8793e289c
Alfers009 <74427063+Alfers009@users.noreply.github.com>
2021-04-14 23:23:38 +0300

    14.4

---
+++ Website buildingg/14-website building/classes.js
@@ -0,0 +1,25 @@
+class Animal {
+    constructor(age){
+        this.age = age;
+}
+
+class Cat {
+
+    }
+
+    funtion cry () {
+        console.log("Miau Miau MIau Miau");
+    }
+}
+
+class RussianBlueCat
+
+
+let pisicaRino = new Cat(2);
+let pisicaTommy = new Cat(2);
+let pisicaTommy2 = new Cat(2);
+let pisicaTommy3 = new Cat(2);
+
+
+pisicaRino.cry();
+pisicaTommy.cry();
